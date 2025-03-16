6b6b9035394c198dbda1f60382a4f4b774b411ee
Stefan Hoelzl <1478183+stefanhoelzl@users.noreply.github.com>
2025-03-16 09:02:13 +0100

    made query endpoint more robust

--- src/routes/api/query/+server.ts
+++ src/routes/api/query/+server.ts
@@ -1,4 +1,4 @@
-import { DuckDBInstance, DuckDBPendingResultState } from "@duckdb/node-api";
+import { DuckDBInstance } from "@duckdb/node-api";
 import { json } from "@sveltejs/kit";
 
 let db: DuckDBInstance | undefined = undefined;
@@ -9,19 +9,18 @@ export async function POST({ request }) {
   if (!db)
     db = await DuckDBInstance.create("/var/home/stefan/Development/repos/digger/tb2logs/db.ddb", {
       access_mode: "READ_ONLY",
-      threads: "2",
-      memory_limit: "4GB"
+      // threads: "2",
+      // memory_limit: "4GB",
     });
   const conn = await db.connect();
   await conn.run("PRAGMA enable_progress_bar");
 
   const prepared = await conn.prepare(query);
-  const pending = prepared.start();
-  while (pending.runTask() !== DuckDBPendingResultState.RESULT_READY) {
-    await new Promise((r) => setTimeout(r, 10));
-  }
-  const result = await pending.getResult();
+  const result = await prepared.run();
   const rows = await result.getRows();
+
+  conn.close();
+
   return json(
     rows.map((row) => row.map((v) => (typeof v === "bigint" ? parseInt(v.toString()) : v)))
   );
