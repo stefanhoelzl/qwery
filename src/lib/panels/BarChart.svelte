e9f64d8b9d3f730b1465b0b2263b0471a7969dcd
Stefan Hoelzl <1478183+stefanhoelzl@users.noreply.github.com>
2025-03-16 08:59:40 +0100

    added bar chart panel

---
+++ src/lib/panels/BarChart.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import * as echarts from "echarts";
+  import { onMount } from "svelte";
+  import type { PanelContext } from "$lib/DashboardManager.svelte";
+  import type { DataField } from "$lib/QueryBuilder";
+
+  interface Props {
+    ctx: PanelContext;
+    x: DataField<unknown>;
+    y: DataField<unknown>;
+  }
+  const { ctx, x, y }: Props = $props();
+
+  let chartElement: HTMLDivElement;
+  let chart: echarts.ECharts;
+
+  function refresh() {
+    ctx.fetch({ x, y }).then((data) => {
+      chart.setOption({
+        xAxis: {
+          data: data.map((r) => r.x)
+        },
+        series: [
+          {
+            type: "bar",
+            data: data.map((r) => r.y)
+          }
+        ]
+      });
+    });
+  }
+
+  $effect(() => refresh());
+  ctx.onUpdate(() => refresh());
+
+  onMount(() => {
+    chart = echarts.init(chartElement);
+    chart.setOption({
+      xAxis: { data: [] },
+      yAxis: {},
+      tooltip: {},
+      series: [],
+      grid: {
+        top: 10,
+        bottom: 20,
+        left: 60,
+        right: 10
+      }
+    });
+
+    const resizeObserver = new ResizeObserver(() => chart.resize());
+    resizeObserver.observe(chartElement);
+  });
+</script>
+
+<div bind:this={chartElement} class="bar"></div>
+
+<style>
+  .bar {
+    background-color: white;
+    height: 100%;
+    width: 100%;
+  }
+</style>
